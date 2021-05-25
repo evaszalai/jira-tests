c60347186149e647cb7c09621818dfc92f064887
evaszalai <szevapianist@gmail.com>
2021-05-25 14:11:30 +0200

    setup: KDT file structure, ReadObject class

---
+++ src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/KDT/operation/UIOperation.java
@@ -0,0 +1,4 @@
+package com.codecool.jira.KDT_and_POM_Jira_tests.KDT.operation;
+
+public class UIOperation {
+}
---
+++ src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/KDT/testCases/CreateIssueTest.java
@@ -0,0 +1,4 @@
+package com.codecool.jira.KDT_and_POM_Jira_tests.KDT.testCases;
+
+public class CreateIssueTest {
+}
