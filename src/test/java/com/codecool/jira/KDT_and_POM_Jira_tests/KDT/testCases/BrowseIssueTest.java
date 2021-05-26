3575915f0f14446db17245fa78fcb909635a96d5
evaszalai <szevapianist@gmail.com>
2021-05-27 01:03:55 +0200

    test: browse issues nr.1, 2, 3 in COALA, JETI and TOUCAN projects

--- src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/KDT/testCases/BrowseIssueTest.java
+++ src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/KDT/testCases/BrowseIssueTest.java
@@ -1,9 +1,7 @@
 package com.codecool.jira.KDT_and_POM_Jira_tests.KDT.testCases;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.codecool.jira.KDT_and_POM_Jira_tests.KDT.operation.UIOperation;
+import org.junit.jupiter.api.*;
 
 public class BrowseIssueTest extends TestBase {
     @BeforeAll
@@ -26,5 +24,55 @@ public class BrowseIssueTest extends TestBase {
     public void browseNonExistentIssue() throws Exception {
         operation.goToCompoundUrl("browseUrlBase", "COALA-5");
         Assertions.assertEquals("You can't view this issue", operation.getText("issueNotAvailable", "xpath"));
+    }
+
+    @Test
+    public void browseCoalaIssue1() throws Exception {
+        operation.browseIssue("COALA-1");
+    }
+
+    @Test
+    public void browseCoalaIssue2() throws Exception {
+        operation.browseIssue("COALA-2");
+    }
+
+    @Test
+    public void browseCoalaIssue3() throws Exception {
+        operation.browseIssue("COALA-3");
+    }
+
+    @Test
+    public void browseJetiIssue1() throws Exception {
+        operation.browseIssue("JETI-1");
+    }
+
+    @Test
+    public void browseJetiIssue2() throws Exception {
+        operation.browseIssue("JETI-2");
+    }
+
+    @Test
+    public void browseJetiIssue3() throws Exception {
+        operation.browseIssue("JETI-3");
+    }
+
+    @Test
+    public void browseToucanIssue1() throws Exception {
+        operation.browseIssue("TOUCAN-1");
+    }
+
+    @Test
+    public void browseToucanIssue2() throws Exception {
+        operation.browseIssue("TOUCAN-2");
+    }
+
+    @Test
+    public void browseToucanIssue3() throws Exception {
+        operation.browseIssue("TOUCAN-3");
+    }
+
+    @AfterEach
+    public void closeBrowser(){
+        UIOperation.quitDriver();
     }
 }
