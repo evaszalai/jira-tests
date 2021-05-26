eff7fc8556ca09daa53143d23ed2f3e132d455f6
evaszalai <szevapianist@gmail.com>
2021-05-26 11:40:24 +0200

    fix: remove allObjects references from LoginPageTest

--- src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/KDT/testCases/LoginPageTest.java
+++ src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/KDT/testCases/LoginPageTest.java
@@ -33,37 +33,37 @@ public class LoginPageTest {
     }
 
     @BeforeEach
-    public void goToURL() {
+    public void goToURL() throws IOException {
         operation = new UIOperation();
-        operation.goToUrl(allObjects, "loginPageURL");
+        operation.goToUrl("loginPageURL");
     }
 
     @Test
     public void invalidPassword() throws Exception {
-        operation.setText(allObjects, "username", "id", username);
-        operation.setText(allObjects, "password", "id", "invalid");
-        operation.click(allObjects, "loginFormSubmit", "id");
-        Assertions.assertEquals("Sorry, your username and password are incorrect - please try again.", operation.getText(allObjects, "loginPageMessage", "xpath"));
-        operation.setText(allObjects, "username", "id", username);
-        operation.setText(allObjects, "password", "id", password);
-        operation.click(allObjects, "loginFormSubmit", "id");
-        operation.click(allObjects, "profilePicture", "xpath");
-        operation.click(allObjects, "viewProfile", "id");
-        Assertions.assertEquals(username, operation.getText(allObjects, "name", "id"));
-        operation.click(allObjects, "profile", "xpath");
-        operation.click(allObjects, "logoutButton", "id");
+        operation.setText("username", "id", username);
+        operation.setText("password", "id", "invalid");
+        operation.click("loginFormSubmit", "id");
+        Assertions.assertEquals("Sorry, your username and password are incorrect - please try again.", operation.getText("loginPageMessage", "xpath"));
+        operation.setText("username", "id", username);
+        operation.setText("password", "id", password);
+        operation.click("loginFormSubmit", "id");
+        operation.click("profilePicture", "xpath");
+        operation.click( "viewProfile", "id");
+        Assertions.assertEquals(username, operation.getText("name", "id"));
+        operation.click( "profile", "xpath");
+        operation.click( "logoutButton", "id");
     }
 
     @Test
     public void successfulLoginAndLogout() throws Exception{
-        operation.setText(allObjects, "username", "id", username);
-        operation.setText(allObjects, "password", "id", password);
-        operation.click(allObjects, "loginFormSubmit", "id");
-        operation.click(allObjects, "profilePicture", "xpath");
-        operation.click(allObjects, "viewProfile", "id");
-        Assertions.assertEquals(username, operation.getText(allObjects, "name", "id"));
-        operation.click(allObjects, "profile", "xpath");
-        operation.click(allObjects, "logoutButton", "id");
+        operation.setText("username", "id", username);
+        operation.setText( "password", "id", password);
+        operation.click("loginFormSubmit", "id");
+        operation.click("profilePicture", "xpath");
+        operation.click("viewProfile", "id");
+        Assertions.assertEquals(username, operation.getText("name", "id"));
+        operation.click("profile", "xpath");
+        operation.click("logoutButton", "id");
     }
 
     @AfterEach
