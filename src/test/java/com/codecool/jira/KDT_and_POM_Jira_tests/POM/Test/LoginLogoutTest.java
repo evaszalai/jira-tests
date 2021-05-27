634d3022a636d4022b82e50e0de469e5914aafb0
evaszalai <szevapianist@gmail.com>
2021-05-27 11:17:52 +0200

    feat: create TestBase abstract parent class

--- src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/POM/Test/LoginLogoutTest.java
+++ src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/POM/Test/LoginLogoutTest.java
@@ -1,4 +1,5 @@
 package com.codecool.jira.KDT_and_POM_Jira_tests.POM.Test;
 
 public class LoginLogoutTest {
+
 }
---
+++ src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/POM/Test/TestBase.java
@@ -0,0 +1,34 @@
+package com.codecool.jira.KDT_and_POM_Jira_tests.POM.Test;
+
+import com.codecool.jira.KDT_and_POM_Jira_tests.KDT.operation.UIOperation;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public abstract class TestBase {
+    public static WebDriver driver;
+    public static String username;
+    public static String password;
+
+    public static void setup() {
+        Properties browserProps = new Properties();
+        try {
+            String browserConfigPath = "settings.properties";
+            browserProps.load(new FileInputStream(browserConfigPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        username = browserProps.getProperty("username");
+        password = browserProps.getProperty("password");
+        String webdriverPath = browserProps.getProperty("webdriver");
+        System.setProperty("webdriver.gecko.driver", webdriverPath);
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setCapability("marionette", true);
+        driver = new FirefoxDriver(firefoxOptions);
+    }
+
+}
