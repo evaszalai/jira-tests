e2700c0c72a1fc2e27ac2b2cf45bee1e9a4236cd
evaszalai <szevapianist@gmail.com>
2021-05-27 17:22:43 +0200

    test: create issue general

--- src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/POM/Pages/CreateIssueScreen.java
+++ src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/POM/Pages/CreateIssueScreen.java
@@ -1,9 +1,14 @@
 package com.codecool.jira.KDT_and_POM_Jira_tests.POM.Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class CreateIssueScreen {
     WebDriver driver;
@@ -31,6 +36,7 @@ public class CreateIssueScreen {
 
     public CreateIssueScreen(WebDriver driver){
         this.driver = driver;
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS) ;
         PageFactory.initElements(driver, this);
     }
 
@@ -60,5 +66,17 @@ public class CreateIssueScreen {
 
     public void clickOnNotification(){
         notification.click();
+    }
+
+    public void clickProjectField(){
+        projectField.click();
+    }
+
+    public void clickIssueTypeField(){
+        issueTypeField.click();
+    }
+
+    public void clickSummaryField(){
+        summaryField.click();
     }
 }
--- src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/POM/Test/CreateIssueTest.java
+++ src/test/java/com/codecool/jira/KDT_and_POM_Jira_tests/POM/Test/CreateIssueTest.java
@@ -1,4 +1,56 @@
 package com.codecool.jira.KDT_and_POM_Jira_tests.POM.Test;
 
-public class CreateIssueTest {
+import com.codecool.jira.KDT_and_POM_Jira_tests.POM.Pages.BrowseIssuePage;
+import com.codecool.jira.KDT_and_POM_Jira_tests.POM.Pages.CreateIssueScreen;
+import com.codecool.jira.KDT_and_POM_Jira_tests.POM.Pages.JiraLogin;
+import com.codecool.jira.KDT_and_POM_Jira_tests.POM.Pages.NavBar;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CreateIssueTest extends TestBase {
+    CreateIssueScreen screen;
+    NavBar navBar;
+    JiraLogin login;
+    BrowseIssuePage issue;
+
+    @BeforeAll
+    public static void start(){
+        setup();
+    }
+
+    @BeforeEach
+    public void login(){
+        driver.get("https://jira-auto.codecool.metastage.net");
+        login = new JiraLogin(driver);
+        login.login(username, password);
+    }
+
+    @Test
+    public void testCreateIssue(){
+        String project = "Main Testing Project (MTP)";
+        String issueType = "Bug";
+        String summary = "Test create issue";
+        navBar = new NavBar(driver);
+        navBar.clickCreateButton();
+        screen = new CreateIssueScreen(driver);
+        screen.clickProjectField();
+        screen.setProject("");
+        screen.setProject(project);
+        screen.clickOutSide();
+        screen.clickIssueTypeField();
+        screen.setIssueType("");
+        screen.setIssueType(issueType);
+        screen.clickOutSide();
+        screen.clickSummaryField();
+        screen.setSummary(summary);
+        screen.clickSubmit();
+        screen.clickOnNotification();
+        issue = new BrowseIssuePage(driver);
+        Assertions.assertEquals(summary, issue.getSummary());
+        Assertions.assertEquals(issueType, issue.getIssueType());
+        Assertions.assertEquals("Main Testing Project", issue.getProjectName());
+    }
+
 }
